5df1f14b59e5471faea427c2bf84f93617784210
Passionfruit715 <935522324@qq.com>
2025-04-14 18:36:05 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Filename: main.py
+Description: test the convergence on multi-echelon inventory model's convergence on base stock policy.
+Course: ISyE 8803 - Learning and Optimization in Operations
+Contributors: Jinghua Weng, Yitong Wu
+Created: 2025-4-14, Jinghua Weng
+""" 
+import numpy as np 
+import math 
+
+
+
